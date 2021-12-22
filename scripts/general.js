016807e27777ee0dc70a31d950944aec6fc948cc
NewGuyAlbert <albe9220@stud.kea.dk>
2021-12-22 21:18:32 +0100

    fix navbar 2

--- scripts/general.js
+++ scripts/general.js
@@ -2,9 +2,9 @@ $(function () {
     const origin = window.location.origin;
     if(origin === "http://localhost"){
         $("a[href='/']").attr("href", "/WebDevFinal/")
-        $("a[href='./views/admin.php']").attr("href", "/WebDevFinal/views/admin.php")
-        $("a[href='./views/signup.php']").attr("href", "/WebDevFinal/views/signup.php")
-        $("a[href='./views/login.php']").attr("href", "/WebDevFinal/views/login.php")
+        $("a[href='./admin.php']").attr("href", "/WebDevFinal/views/admin.php")
+        $("a[href='./signup.php']").attr("href", "/WebDevFinal/views/signup.php")
+        $("a[href='./login.php']").attr("href", "/WebDevFinal/views/login.php")
     }
 
 })
--- views/navbar.php
+++ views/navbar.php
@@ -1,8 +1,8 @@
 <div class="nav">
     <ul>
         <li class="Index"><a class="active" href="/">Home</a></li>
-        <li class="Admin"><a href="./views/admin.php">Admin</a></li>
-        <li class="Signup"><a href="./views/signup.php">Signup</a></li>
-        <li class="Login"><a href="./views/login.php">Login</a></li>
+        <li class="Admin"><a href="./admin.php">Admin</a></li>
+        <li class="Signup"><a href="./signup.php">Signup</a></li>
+        <li class="Login"><a href="./login.php">Login</a></li>
     </ul>
 </div>
